b9839d0f8c59f4faa562aff8916884871421e742
Noah Brenner <noah@noahbrenner.com>
2020-05-24 16:48:30 -0700

    Validate parameters that must be ISO date strings

--- mod.ts
+++ mod.ts
@@ -1,5 +1,30 @@
+export class DateParseError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'DateParseError';
+  }
+}
+
 function localDateToUTC(d: Date) {
   return new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+}
+
+function parseDate(isoDate: string) {
+  const isoDatePattern = /^[0-9]{4}-[0-1][0-9]-[0-3][0-9]$/;
+
+  if (!isoDatePattern.test(isoDate)) {
+    throw new DateParseError(
+      `Invalid date format: "${isoDate}". Please specify as "yyyy-mm-dd"`
+    );
+  }
+
+  const timestamp = Date.parse(isoDate);
+
+  if (isNaN(timestamp)) {
+    throw new DateParseError(`Invalid date: "${isoDate}"`);
+  }
+
+  return new Date(timestamp);
 }
 
 function main(date1: string, date2?: string) {
@@ -8,10 +33,10 @@ function main(date1: string, date2?: string) {
 
   if (date2 === undefined) {
     start = localDateToUTC(new Date());
-    end = new Date(Date.parse(date1));
+    end = parseDate(date1);
   } else {
-    start = new Date(Date.parse(date1));
-    end = new Date(Date.parse(date2));
+    start = parseDate(date1);
+    end = parseDate(date2);
   }
 
   const MS_PER_DAY = 1000 * 60 * 60 * 24;
@@ -22,5 +47,14 @@ function main(date1: string, date2?: string) {
 }
 
 if (import.meta.main) {
-  console.log(main('2020-01-01'));
+  try {
+    console.log(main('2020-01-01'));
+  } catch (error) {
+    if (error instanceof DateParseError) {
+      console.error(error.message);
+      Deno.exit(1);
+    } else {
+      throw error;
+    }
+  }
 }
