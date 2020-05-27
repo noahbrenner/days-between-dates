64e03c9ad10352070bde51c9680fba63a4f312a9
Noah Brenner <noah@noahbrenner.com>
2020-05-27 11:11:13 -0700

    Validate command line arguments

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 days-between-dates
 ==================
 
-A simple CLI application for calculating the number of days between 2 dates, intended to run with [Deno](https://deno.land/).
+A simple CLI utility for calculating the number of days between 2 dates, intended to run with [Deno](https://deno.land/).
--- cli.ts
+++ cli.ts
@@ -1,14 +1,63 @@
 import {DateParseError, daysBetweenDates} from './mod.ts';
 
-if (import.meta.main) {
+const help = `Calculate the number of days between 2 ISO-formatted dates.
+
+USAGE:
+    $exe [start-date] <end-date>
+
+    \`start-date\` defaults to today.
+
+EXAMPLE:
+    # Days between Deno v0.0.1 and v1.0.0
+    $exe 2018-08-17 2020-05-13
+`;
+
+interface CLIOutput {
+  message: string;
+  exitCode: 0 | 1;
+}
+
+function main(args: string[]): CLIOutput {
+  if (args.includes('-h') || args.includes('--help')) {
+    return {
+      message: help,
+      exitCode: 0,
+    };
+  }
+
+  if (args.length < 1 || args.length > 2) {
+    return {
+      message: help,
+      exitCode: 1,
+    };
+  }
+
+  let days: number;
   try {
-    console.log(daysBetweenDates('2020-01-01'));
+    days = daysBetweenDates(...args as [string, string?]);
   } catch (error) {
     if (error instanceof DateParseError) {
-      console.error(error.message);
-      Deno.exit(1);
+      return {
+        message: error.message,
+        exitCode: 1,
+      };
     } else {
       throw error;
     }
   }
+
+  return {
+    message: `${days} days`,
+    exitCode: 0,
+  };
+}
+
+if (import.meta.main) {
+  const {message, exitCode} = main(Deno.args);
+  const consoleFn = exitCode === 0
+    ? 'log'
+    : 'error';
+
+  console[consoleFn](message);
+  Deno.exit(exitCode);
 }
